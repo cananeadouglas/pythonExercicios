8af3db573db809643b2ce3c8bea2c454cce1d3dd
Douglas Cananéa <cananeadouglas@gmail.com>
2023-02-01 20:58:48 -0300

    -> Enviado no dia 2023.02.01 para o repositório online.

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ TAG=$(shell date +%Y.%m.%d)
 add:	
 	git add .
 commit: add
-	git commit -m "httpx -> Enviado no dia $(TAG) para o repositório online."
+	git commit -m "$1 -> Enviado no dia $(TAG) para o repositório online."
 push: 	commit
 	git push
--- httpx_moeda/main.py
+++ httpx_moeda/main.py
@@ -1,17 +1,21 @@
 import httpx
 
 while True: 
-    base_currency = input('digite a moeda base: ').upper()
-    corrente = input('digite a moeda desejada: ').upper()
+    #base_currency = input('digite a moeda base: ').upper()
+    #corrente = input('digite a moeda desejada: ').upper()
+    
+    base = input('ditie a moeda seguida de : ').upper()
+    
+    ticker = base.split(':')
 
     response = httpx.get(
-        url=f'https://api.exchangerate.host/latest?base={base_currency}'
+        url=f'https://api.exchangerate.host/latest?base={ticker[0]}'
         
     ).json()
 
     currency_data = response['rates']
     print ('                           ')
 
-    print (f'1 {base_currency} vale {currency_data.get(corrente)} {corrente}')
+    print (f'1 {ticker[0]} vale {currency_data.get(ticker[1])} {ticker[1]}')
     print ('---------------------------')
     print ('                           ')
